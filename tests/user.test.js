4a6c7d105bf3f3da4d6252b6072cfc234ca72aaa
devleopar <oktarkara@gmail.com>
2020-08-16 22:19:56 +0300

    login and create requests tested

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,10 +1,39 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
 
-test('Should signup a new user', async () =>{
+const userOne = {
+    name: 'Mike',
+    email: 'mike@example.com',
+    password: '56what!!'
+}
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
+
+
+
+
+test('Should signup a new user', async () => {
     await request(app).post('/users').send({
-        name:'Andrew',
+        name: 'Andrew',
         email: 'andrew@example.com',
         password: 'MyPass777!'
     }).expect(201)
+})
+
+test('Should login existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('Should not login nonexistent user', async () => {
+    await request(app).post('/users/login').send({
+        email: "james@example.com",
+        password: "bond"
+    }).expect(400)
 })
